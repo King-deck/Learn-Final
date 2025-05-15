5d4e0fa0db3bf9062f9248b018981c34a53ade9b
King-deck <poreliphone15@gmail.com>
2025-05-14 21:03:05 -0500

    chore: Reemplaza las imágenes con Image1.png y Image2.png

--- Learnsy/Learn.js
+++ Learnsy/Learn.js
@@ -1,5 +1,6 @@
 function handleScrollReveal() {
   const elementsToReveal = document.querySelectorAll('.reveal, .zoom-reveal, .pilares-zigzag .pilar-card, .mentors-section .mentor-card, .cursos-section-aesthetic, .cursos-section-learnsy, .testimonios-section, .nueva-seccion-learnsy, .es-hora-de-cambiar-section');
+  const equipoSection = document.querySelector('.equipo-section');
 
   elementsToReveal.forEach(el => {
     const rect = el.getBoundingClientRect();
@@ -11,6 +12,18 @@ function handleScrollReveal() {
       el.classList.remove('active');
     }
   });
+
+  // Lógica específica para la sección del equipo
+  if (equipoSection) {
+    const rectEquipo = equipoSection.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    // Añade la clase 'active' si la parte superior del elemento entra en la vista
+    if (rectEquipo.top < windowHeight * 0.85) {
+      equipoSection.classList.add('active');
+    }
+    // No removemos la clase 'active' para la sección del equipo
+  }
 }
 
 window.addEventListener('scroll', handleScrollReveal);
